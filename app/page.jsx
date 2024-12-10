98fc433b4a11f0bbb3f3fc24fe33203a474656e3
Samin Labbaf <samin.labbaf@georgebrown.ca>
2024-12-10 18:44:54 -0500

    home page hero section

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,21 @@
+import Image from "next/image";
+import hero from '../public/images/hero.png'
+import './styles/home.styles.css';
+
 export default function Page() {
-    return <>
-    </>
-  }
+  return (
+    <main>
+      <div className='imageContainer'>
+        <Image
+          layout="fill" // Fills the container
+          objectFit="cover" // Maintains aspect ratio, cropping if necessary
+          priority // Optional for faster loading (useful for above-the-fold images)
+          src={hero} alt='persian food hero image' />
+      </div>
+      <div className="heroText">
+        <h1 className='heroTitle'>Bring Healthy Cooking and Joy To Your Home With Me ;)</h1>
+        <h5 className="heroDesc">Browse through a variety of recipes across different categories</h5>
+      </div>
+    </main>
+  )
+}
